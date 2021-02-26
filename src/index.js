8e03162c474ab13004be3994a0c1aa984782362f
Pat Santucci <pats@usafacts.org>
2021-02-26 11:33:14 -0800

    Refactoring to use functional components

--- src/index.js
+++ src/index.js
@@ -1,104 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 
-class CheckersBoard extends React.Component {
-  state = {
-    board: [
-      [0, 1, 0, 1, 0, 1, 0, 1],
-      [1, 0, 1, 0, 1, 0, 1, 0],
-      [0, 1, 0, 1, 0, 1, 0, 1],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [2, 0, 2, 0, 2, 0, 2, 0],
-      [0, 2, 0, 2, 0, 2, 0, 2],
-      [2, 0, 2, 0, 2, 0, 2, 0]
-    ]
-  };
+const CheckersBoard = ({ size }) => {
+  const [board, setBoard] = useState([
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [2, 0, 2, 0, 2, 0, 2, 0],
+    [0, 2, 0, 2, 0, 2, 0, 2],
+    [2, 0, 2, 0, 2, 0, 2, 0]
+  ]);
 
-  render() {
-    const spaceSize = this.props.size / 8;
-    const pieceRadius = spaceSize / 2;
+  const spaceSize = size / 8;
+  const pieceRadius = spaceSize / 2;
 
-    return (
-      <svg
-        height={this.props.size}
-        width={this.props.size}
-        viewBox={`0 0 ${this.props.size} ${this.props.size}`}
-      >
-        {this.state.board.map((row, y) => {
-          const isEvenRow = y % 2;
-          const spaceY = spaceSize * y;
+  return (
+    <svg height={size} width={size} viewBox={`0 0 ${size} ${size}`}>
+      {board.map((row, y) => {
+        const isEvenRow = y % 2;
+        const spaceY = spaceSize * y;
 
-          return row.map((space, x) => {
-            const isEvenSpace = x % 2;
-            const spaceX = spaceSize * x;
+        return row.map((space, x) => {
+          const isEvenSpace = x % 2;
+          const spaceX = spaceSize * x;
 
-            return (
-              <Space
-                key={x}
-                shade={
-                  (isEvenSpace && !isEvenRow) || (!isEvenSpace && isEvenRow)
-                }
-                size={spaceSize}
-                x={spaceX}
-                y={spaceY}
-              />
-            );
-          });
-        })}
-        {this.state.board.map((row, y) => {
-          const spaceY = spaceSize * y;
+          return (
+            <Space
+              key={x}
+              shade={(isEvenSpace && !isEvenRow) || (!isEvenSpace && isEvenRow)}
+              size={spaceSize}
+              x={spaceX}
+              y={spaceY}
+            />
+          );
+        });
+      })}
+      {board.map((row, y) => {
+        const spaceY = spaceSize * y;
 
-          return row.map((space, x) => {
-            const spaceX = spaceSize * x;
+        return row.map((space, x) => {
+          const spaceX = spaceSize * x;
 
-            if (space === 0) {
-              // The space is empty.
-              return null;
-            }
+          if (space === 0) {
+            // The space is empty.
+            return null;
+          }
 
-            return (
-              <Piece
-                key={x}
-                centerX={spaceX + pieceRadius}
-                centerY={spaceY + pieceRadius}
-                player={space}
-                radius={pieceRadius * 0.75}
-              />
-            );
-          });
-        })}
-      </svg>
-    );
-  }
-}
+          return (
+            <Piece
+              key={x}
+              centerX={spaceX + pieceRadius}
+              centerY={spaceY + pieceRadius}
+              player={space}
+              radius={pieceRadius * 0.75}
+            />
+          );
+        });
+      })}
+    </svg>
+  );
+};
 
-class Space extends React.Component {
-  render() {
-    return (
-      <rect
-        fill={this.props.shade ? "green" : "lightgray"}
-        height={this.props.size}
-        width={this.props.size}
-        x={this.props.x}
-        y={this.props.y}
-      />
-    );
-  }
-}
+const Space = ({ shade, size, x, y }) => {
+  return (
+    <rect
+      fill={shade ? "green" : "lightgray"}
+      height={size}
+      width={size}
+      x={x}
+      y={y}
+    />
+  );
+};
 
-class Piece extends React.Component {
-  render() {
-    return (
-      <circle
-        cx={this.props.centerX}
-        cy={this.props.centerY}
-        fill={this.props.player === 1 ? "white" : "red"}
-        r={this.props.radius}
-      />
-    );
-  }
-}
+const Piece = ({ centerX, centerY, player, radius }) => {
+  return (
+    <circle
+      cx={centerX}
+      cy={centerY}
+      fill={player === 1 ? "white" : "red"}
+      r={radius}
+    />
+  );
+};
 
 const container = document.createElement("div");
 document.body.appendChild(container);
