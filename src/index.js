0e7688f4465455ffa884c93641c1af433352df0c
Sabrina Pearson <sabrip2@cs.washington.edu>
2021-03-03 10:08:26 -0800

    updated app

--- src/index.js
+++ src/index.js
@@ -13,57 +13,279 @@ const CheckersBoard = ({ size }) => {
     [2, 0, 2, 0, 2, 0, 2, 0]
   ]);
 
+  const [xPrev, setX] = useState(-1);
+  const [yPrev, setY] = useState(-1);
+  const [curPlayer, setPlayer] = useState(1);
+  const [redScore, setRed] = useState(0);
+  const [whiteScore, setWhite] = useState(0);
+  const [doubleJump, setJump] = useState(false);
+
   const spaceSize = size / 8;
   const pieceRadius = spaceSize / 2;
 
-  return (
-    <svg height={size} width={size} viewBox={`0 0 ${size} ${size}`}>
-      {board.map((row, y) => {
-        const isEvenRow = y % 2;
-        const spaceY = spaceSize * y;
+  const handleClickCircle = (row, column) => {
+    let selected = board[column][row];
+    if (selected > 2) {
+      selected = Math.floor(selected / 2);
+    }
+    if (selected != curPlayer) {
+      alert("It is not your turn to move");
+      return;
+    }
+    if (doubleJump && row != xPrev && column != yPrev) {
+      alert("You must double jump");
+      return;
+    }
+    setX(row);
+    setY(column);
+    hasMoves(column, row);
+  };
 
-        return row.map((space, x) => {
-          const isEvenSpace = x % 2;
-          const spaceX = spaceSize * x;
+  function move(x1, y1, x2, y2) {
+    let copy = [...board];
+    let temp = copy[y1][x1];
+    copy[y1][x1] = 0;
+    copy[y2][x2] = temp;
+    setBoard(copy);
+    checkKing(x2, y2);
+    reset();
+  }
 
-          return (
-            <Space
-              key={x}
-              shade={(isEvenSpace && !isEvenRow) || (!isEvenSpace && isEvenRow)}
-              size={spaceSize}
-              x={spaceX}
-              y={spaceY}
-            />
-          );
-        });
-      })}
-      {board.map((row, y) => {
-        const spaceY = spaceSize * y;
+  function reset() {
+    setX(-1);
+    setY(-1);
+    checkWin();
+    setPlayer(curPlayer == 1 ? 2 : 1);
+  }
+  function checkKing(x, y) {
+    if (curPlayer == 1) {
+      if (y == board.length - 1) {
+        let copy = [...board];
+        copy[y][x] = 3;
+        setBoard(copy);
+      }
+    } else if (curPlayer == 2) {
+      if (y == 0) {
+        let copy = [...board];
+        copy[y][x] = 4;
+        setBoard(copy);
+      }
+    }
+  }
 
-        return row.map((space, x) => {
-          const spaceX = spaceSize * x;
+  function jump(x1, y1, x2, y2) {
+    let midy = (y1 + y2) / 2;
+    let midx = (x1 + x2) / 2;
+    let copy = [...board];
+    let temp = copy[y1][x1];
+    copy[y1][x1] = 0;
+    copy[midy][midx] = 0;
+    copy[y2][x2] = temp;
+    checkKing(x2, y2);
+    setBoard(copy);
 
-          if (space === 0) {
-            // The space is empty.
-            return null;
+    if (canJump(y2, x2)) {
+      setX(x2);
+      setY(y2);
+      setJump(true);
+    } else {
+      setJump(false);
+      reset();
+    }
+  }
+
+  function checkWin() {
+    var opponent = curPlayer == 1 ? 2 : 1;
+    // check if captured all the pieces
+    if (redScore == 12) {
+      alert("Red Won");
+    } else if (whiteScore == 12) {
+      alert("White Won");
+    } else {
+      // Check if opponent has any moves
+      for (var r = 0; r < board.length; r++) {
+        for (var c = 0; c < board[0].length; c++) {
+          if (
+            (board[r][c] == opponent || board[r][c] == opponent + 2) &&
+            hasMoves(r, c)
+          ) {
+            return true;
           }
+        }
+      }
+      alert("Player " + curPlayer + " has won");
+    }
+  }
+  function canJumpOver(over, under) {
+    if ((over == 1 || over == 3) && (under == 2 || under == 4)) {
+      return true;
+    } else if ((over == 2 || over == 4) && (under == 1 || under == 3)) {
+      return true;
+    }
+    return false;
+  }
+  function canJump(x, y) {
+    // check pieces that can jump up (1,3,4)
+    var selected = board[x][y];
+    var under = 0;
+    if (selected != 2 && x + 2 < board.length) {
+      // check down left
+      if (y - 2 >= 0 && board[x + 2][y - 2] == 0) {
+        under = board[x + 1][y - 1];
+        if (canJumpOver(selected, under)) {
+          return true;
+        }
+      }
+      // check down right
+      if (y + 2 < board.length && board[x + 2][y + 2] == 0) {
+        under = board[x + 1][y + 1];
+        if (canJumpOver(selected, under)) {
+          return true;
+        }
+      }
+      // check pieces that can jump up (2,3,4)
+    }
+    if (selected != 1 && x - 2 >= 0) {
+      // up right
+      if (y + 2 < board.length && board[x - 2][y + 2] == 0) {
+        under = board[x - 1][y + 1];
+        if (canJumpOver(selected, under)) {
+          return true;
+        }
+      }
 
-          return (
-            <Piece
-              key={x}
-              centerX={spaceX + pieceRadius}
-              centerY={spaceY + pieceRadius}
-              player={space}
-              radius={pieceRadius * 0.75}
-            />
-          );
-        });
-      })}
-    </svg>
+      // up left
+      if (y - 2 >= 0 && board[x - 2][y - 2] == 0) {
+        under = board[x - 1][y - 1];
+        if (canJumpOver(selected, under)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  // Checks if piece at x and y has a vaild move
+  function hasMoves(x, y) {
+    // checks any piece that can move up (1,3,4)
+    if (
+      board[x][y] != 2 &&
+      x + 1 < board.length &&
+      x - 1 >= 0 &&
+      y + 1 < board.length
+    ) {
+      if (board[x + 1][y - 1] == 0 || board[x + 1][y + 1] == 0) {
+        return true;
+      }
+    }
+    // checks any piece that can move down (2,3,4)
+    if (board[x][y] != 1 && y + 1 < board.length && y - 1 >= 0 && x - 1 >= 0) {
+      if (board[x - 1][y + 1] == 0 || board[x - 1][y - 1] == 0) {
+        return true;
+      }
+    }
+    return canJump(x, y);
+  }
+
+  var handleClickSquare = (row, column) => {
+    if (xPrev == -1 && yPrev == -1) {
+      alert("Select a piece to move first");
+      return;
+    }
+    if (board[column][row] != 0) {
+      alert("This space is taken");
+    } else {
+      let selected = board[yPrev][xPrev];
+
+      let yDis = column - yPrev;
+      let xDis = Math.abs(row - xPrev);
+      // checks if this is a valid 1 step move
+      if (
+        (selected != 2 && yDis == 1 && xDis == 1) ||
+        (selected != 1 && yDis == -1 && xDis == 1)
+      ) {
+        move(xPrev, yPrev, row, column);
+        // checks if this is a valid jump
+      } else if (
+        (selected != 2 && yDis == 2 && xDis == 2) ||
+        (selected != 1 && yDis == -2 && xDis == 2)
+      ) {
+        let midy = (column + yPrev) / 2;
+        let midx = (row + xPrev) / 2;
+        if (canJumpOver(selected, board[midy][midx])) {
+          // updates scores
+          if (curPlayer == 1) {
+            setWhite(whiteScore + 1);
+          } else {
+            setRed(redScore + 1);
+          }
+          jump(xPrev, yPrev, row, column);
+        } else {
+          alert("Invalid Move");
+        }
+      } else {
+        alert("Invalid Move");
+      }
+    }
+  };
+
+  return (
+    <div>
+      <svg height={size} width={size} viewBox={`0 0 ${size} ${size}`}>
+        {board.map((row, y) => {
+          const isEvenRow = y % 2;
+          const spaceY = spaceSize * y;
+
+          return row.map((space, x) => {
+            const isEvenSpace = x % 2;
+            const spaceX = spaceSize * x;
+
+            return (
+              <Space
+                key={x}
+                shade={
+                  (isEvenSpace && !isEvenRow) || (!isEvenSpace && isEvenRow)
+                }
+                size={spaceSize}
+                x={spaceX}
+                y={spaceY}
+                click={() => handleClickSquare(x, y)}
+              />
+            );
+          });
+        })}
+        {board.map((row, y) => {
+          const spaceY = spaceSize * y;
+
+          return row.map((space, x) => {
+            const spaceX = spaceSize * x;
+
+            if (space === 0) {
+              // The space is empty.
+              return null;
+            }
+
+            return (
+              <Piece
+                key={x}
+                centerX={spaceX + pieceRadius}
+                centerY={spaceY + pieceRadius}
+                player={space}
+                radius={pieceRadius * 0.75}
+                click={() => handleClickCircle(x, y)}
+              />
+            );
+          });
+        })}
+      </svg>
+      <p>Current player is : {curPlayer} </p>
+      <p>Player 1 score : {whiteScore} </p>
+      <p>Player 2 score : {redScore} </p>
+    </div>
   );
 };
 
-const Space = ({ shade, size, x, y }) => {
+const Space = ({ shade, size, x, y, click }) => {
   return (
     <rect
       fill={shade ? "green" : "lightgray"}
@@ -71,18 +293,22 @@ const Space = ({ shade, size, x, y }) => {
       width={size}
       x={x}
       y={y}
+      onClick={click}
     />
   );
 };
 
-const Piece = ({ centerX, centerY, player, radius }) => {
+const Piece = ({ centerX, centerY, player, radius, click }) => {
+  var color = "white";
+  if (player == 2) {
+    color = "red";
+  } else if (player == 3) {
+    color = "Grey";
+  } else if (player == 4) {
+    color = "DarkRed";
+  }
   return (
-    <circle
-      cx={centerX}
-      cy={centerY}
-      fill={player === 1 ? "white" : "red"}
-      r={radius}
-    />
+    <circle cx={centerX} cy={centerY} fill={color} r={radius} onClick={click} />
   );
 };
 
